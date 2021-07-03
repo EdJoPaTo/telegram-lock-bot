8442f38fab35168b35289458e31487559377bc0c
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-07-03 12:40:40 +0200

    feat(leave-channel): implement

--- source/parts/index.ts
+++ source/parts/index.ts
@@ -2,9 +2,12 @@ import {Composer} from 'grammy'
 
 import * as commands from './commands.js'
 import * as error from './error.js'
+import * as leaveChannel from './leave-channel.js'
 
 export const bot = new Composer()
 
 bot.use(error.bot.middleware())
+
+bot.use(leaveChannel.bot.middleware())
 
 bot.use(commands.bot.middleware())
---
+++ source/parts/leave-channel.ts
@@ -0,0 +1,14 @@
+import {Composer} from 'grammy'
+
+export const bot = new Composer()
+
+bot.on('channel_post', async ctx => {
+	try {
+		await ctx.reply('Adding a random bot as an admin to your channel is maybe not the best idea…\n\nSincerely, a random bot, added as an admin to this channel.')
+	} catch (error: unknown) {
+		console.log('ERROR channel_post reply', error)
+	}
+
+	console.log('leave the channel…', ctx.chat)
+	await ctx.leaveChat()
+})
